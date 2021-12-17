1c9fba0a9ade2f1feb04597d85c2306e5b1aff4d
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-17 10:54:32 -0300

    Aplicação de efeito de transição

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,11 +1,28 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(-50px , 0)'
+        }),
+        animate('500ms 0s ease-in-out') //duração, delay e aceleração
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado'
 
   constructor() { }
 
--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,7 @@
-import { state, style, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+
+import { Imagem } from './imagem.model';
 
 @Component({
   selector: 'app-banner',
@@ -11,18 +13,46 @@ import { Component, OnInit } from '@angular/core';
         opacity: 0
       })), state('visivel', style({
         opacity: 1
-      })
-      )
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in')),
     ])
   ]
 })
 export class BannerComponent implements OnInit {
 
-  public estado: string = 'escondido'
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png ' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_2.png ' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_3.png ' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_4.png ' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_5.png ' }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => this.logicaRotacao(), 3000)
   }
 
+
+  public logicaRotacao(): void {
+
+    //Auxilia na exibição da imagem seguinte
+    let idx: number = 0
+
+    //Ocultar imagem
+    for (let i: number = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido'
+
+        idx = i === 4 ? 0 : i + 1
+        break
+      }
+    }
+
+    //exibir a próxima imagem
+    this.imagens[idx].estado = 'visivel'
+
+    setTimeout(() => this.logicaRotacao(), 3000)
+  }
 }
