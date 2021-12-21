538a56112700720d480df44ca9149eae88f584a7
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-21 11:27:13 -0300

    Update autenticacao.service.ts

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -3,11 +3,19 @@ import * as firebase from "firebase"
 
 export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario): void {
-        console.log('Chegamos até o serviço: ', usuario)
 
         firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
             .then((resposta: any) => {
-                console.log(resposta)
+
+                //remove a senha do atributo senha do usuario
+                // o comando abaixo serve para ignorar o erro que fere o atributo da interface criada
+                // @ts-expect-error Aqui vai ocorrer um erro, mas estou ignorando  
+                delete usuario.senha
+
+                //registra dados complementares no path email na base64
+                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+                    .set(usuario)
+
             })
             .catch((error: Error) => {
                 console.log(error)
