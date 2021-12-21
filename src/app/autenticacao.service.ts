b850c4813b141ba05f78b964ab758256e92670d1
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-21 15:35:56 -0300

    Feedback para cadastro de usuário

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -2,9 +2,9 @@ import { Usuario } from "./acesso/usuario.model"
 import * as firebase from "firebase"
 
 export class Autenticacao {
-    public cadastrarUsuario(usuario: Usuario): void {
+    public cadastrarUsuario(usuario: Usuario): Promise<any> {
 
-        firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+        return firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
             .then((resposta: any) => {
 
                 //remove a senha do atributo senha do usuario
