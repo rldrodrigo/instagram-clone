36e4483ac50fc74b61243b3fbca4499fc3d3b669
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-28 15:08:38 -0300

    Criando um modal para publicações

---
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-incluir-publicacao',
+  templateUrl: './incluir-publicacao.component.html',
+  styleUrls: ['./incluir-publicacao.component.css']
+})
+export class IncluirPublicacaoComponent implements OnInit {
+
+  public formulario: FormGroup = new FormGroup(
+    {
+      'titulo': new FormControl(null)
+    }
+  )
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public publicar(): void {
+    console.log('Chegamos até aqui!! ')
+  }
+
+}
