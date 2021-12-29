60a0843cefd5d9cd5f0593af6c2d6e2ef6998501
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-29 11:29:41 -0300

    Controlando o progresso de upload

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -10,6 +10,19 @@ export class Bd {
         firebase.storage().ref()
             .child(`imagens/${nomeImagem}`)
             .put(publicacao.imagem)
+            .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                //Acompanhamento do progresso do upload
+                (snapshot: any) => {
+                    console.log(snapshot)
+                },
+                (error) => {
+                    console.log(error)
+                },
+                () => {
+                    //finalização do processo
+                    console.log('upload completo')
+                }
+            )
         /*
         firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
             .push({ titulo: publicacao.titulo })
