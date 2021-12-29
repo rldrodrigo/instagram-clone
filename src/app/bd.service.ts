93ffa1f8b7b62653ac32a8c0164c78f3283649f4
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-29 11:07:56 -0300

    Realizando o upload de imagens no firebase

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -4,8 +4,16 @@ export class Bd {
     public publicar(publicacao: any): void {
 
         console.log(publicacao)
+
+        let nomeImagem = Date.now()
+
+        firebase.storage().ref()
+            .child(`imagens/${nomeImagem}`)
+            .put(publicacao.imagem)
+        /*
         firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
             .push({ titulo: publicacao.titulo })
-        console.log('Chegamos até o serviço responsável pelo controle de dados')
+        */
+
     }
 }
--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -12,6 +12,7 @@ import { Bd } from 'src/app/bd.service';
 export class IncluirPublicacaoComponent implements OnInit {
 
   public email: string = ''
+  private imagem: any
 
   public formulario: FormGroup = new FormGroup(
     {
@@ -33,12 +34,13 @@ export class IncluirPublicacaoComponent implements OnInit {
   public publicar(): void {
     this.bd.publicar({
       email: this.email,
-      titulo: this.formulario.value.titulo
+      titulo: this.formulario.value.titulo,
+      imagem: this.imagem[0]
     })
   }
 
   public preparaImagemUpload(event: Event): void {
-    console.log((<HTMLInputElement>event.target).files)
+    this.imagem = (<HTMLInputElement>event.target).files
   }
 
 }
