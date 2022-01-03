4a18f6d3119398761aad73d3e104694ea5e8534d
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-01-03 12:20:49 -0300

    Associando upload da imagem (Storage) e titulo (database)

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -9,34 +9,28 @@ export class Bd {
 
     public publicar(publicacao: any): void {
 
-        //console.log(publicacao)
-
-        let nomeImagem = Date.now()
-
-        firebase.storage().ref()
-            .child(`imagens/${nomeImagem}`)
-            .put(publicacao.imagem)
-            .on(firebase.storage.TaskEvent.STATE_CHANGED,
-                //Acompanhamento do progresso do upload
-                (snapshot: any) => {
-                    this.progresso.status = 'andamento'
-                    this.progresso.estado = snapshot
-
-                },
-                (error) => {
-                    this.progresso.status = 'erro'
-
-                },
-                () => {
-                    //finalização do processo
-                    this.progresso.status = 'concluido'
-
-                }
-            )
-        /*
         firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
             .push({ titulo: publicacao.titulo })
-        */
+            .then((resposta: any) => {
+                let nomeImagem = resposta.key
 
+                firebase.storage().ref()
+                    .child(`imagens/${nomeImagem}`)
+                    .put(publicacao.imagem)
+                    .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                        //Acompanhamento do progresso do upload
+                        (snapshot: any) => {
+                            this.progresso.status = 'andamento'
+                            this.progresso.estado = snapshot
+                        },
+                        (error) => {
+                            this.progresso.status = 'erro'
+                        },
+                        () => {
+                            //finalização do processo
+                            this.progresso.status = 'concluido'
+                        }
+                    )
+            })
     }
 }
